cc35250cc9e6c6119296f82fb182ec64f4036f3d
Nolan Seokane <nolan.seokane@wunderthompson.com>
2022-08-01 12:22:33 +0200

    complete UI (no javascript functionality)

---
+++ phonebook.js
@@ -0,0 +1,11 @@
+let contacts = [
+	{
+		"id": 1,
+		"first_name": "John",
+		"last_name": "Doe",
+		"phone_number": "+27840000000",
+		"email": "johndeo@gmail.com",
+		"profile_image": "https://cdn.imgbin.com/6/25/24/imgbin-user-profile-computer-icons-user-interface-mystique-aBhn3R8cmqmP4ECky4DA3V88y.jpg"
+	}
+]
+
